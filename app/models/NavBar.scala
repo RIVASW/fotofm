a6c32fbbd3f77affbb515fdc2afd7b5ab03be4c3
Ivan Romanyuk <ivanvl@gmail.com>
2016-01-02 19:40:09 +0600

    New menu item added (tranings)

--- app/models/NavBar.scala
+++ app/models/NavBar.scala
@@ -7,5 +7,5 @@ case object NavBar {
   var NavItems = List( ("Дети", "/section/children.html"), ("Семья", "/section/family.html"),
     ("События", "/section/events.html"), ("Персональное", "/section/personal.html"),
     ("В ожидании", "/section/waitingforbaby.html"), ("Свадебное", "/section/wedding.html"),
-    ("Контакты", "/static/contacts.html"))
+    ("Обучение", "/static/traning.html"), ("Контакты", "/static/contacts.html"))
 }
